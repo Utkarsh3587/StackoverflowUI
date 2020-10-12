e8e6e5a77de6b33503ec0bb84c8440187dee7f78
Utkarsh Soni <soniutkarsh91@gmail.com>
2020-10-13 00:24:58 +0530

    feat: Add Pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +22,7 @@ import { QuestionComponent } from './question/question.component';
     ToastrModule.forRoot(),
     HttpClientModule,
     BrowserModule,
+    InfiniteScrollModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule
